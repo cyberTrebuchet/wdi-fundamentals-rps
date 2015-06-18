8d1ffec0ceeb77a3d6ec651b12e752a423b34b1f
T3 <T3@Alejandros-MBP.home>
2015-06-18 12:35:38 -0400

    Finished. Changed playToFive() to playToX(times), changing the for loop's conditional from 5 to times.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -53,7 +53,7 @@ function getWinner(playerMove,computerMove) {
         case 'rock':
             switch (computerMove) {
                 case 'rock':
-                    winner = 'tie';
+                    winner = 'nobody';
                     break;
                 case 'paper':
                     winner = 'computer';
@@ -69,7 +69,7 @@ function getWinner(playerMove,computerMove) {
                     winner = 'player';
                     break;
                 case 'paper':
-                    winner = 'tie';
+                    winner = 'nobody';
                     break;
                 case 'scissors':
                     winner = 'computer';
@@ -85,7 +85,7 @@ function getWinner(playerMove,computerMove) {
                     winner = 'player';
                     break;
                 case 'scissors':
-                    winner = 'tie';
+                    winner = 'nobody';
                     break;
             }
             break;
@@ -94,15 +94,34 @@ function getWinner(playerMove,computerMove) {
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
+    console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove + '. ' + winner.charAt(0).toUpperCase() + winner.slice(1) + ' is the winner!');
     return winner;
 }
 
-function playToFive() {
+function playToX(times) {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
+    var ties = 0
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
+    for (var i = 0; i < times; i++) {
+        var winner = getWinner(getPlayerMove(getInput()), getComputerMove(randomPlay()));
+        switch (winner) {
+            case 'player':
+                playerWins++;
+                break;
+            case 'computer':
+                computerWins++;
+                break;
+            case 'nobody':
+                ties++;
+                break;
+            default:
+                console.log('Huh?');
+        }
+    }
+    console.log('The score is currently ' + playerWins + ' to ' + computerWins + ', with ' + ties + (ties > 1 || ties === 0 ? ' ties.' : ' tie.'));
     return [playerWins, computerWins];
 }
 
